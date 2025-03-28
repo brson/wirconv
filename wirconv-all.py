9ad1fcf328edf15da183bb477abf0718f96d903f
Brian Anderson <andersrb@gmail.com>
2025-03-27 22:07:24 -0600

    .

--- wirconv-all.py
+++ wirconv-all.py
@@ -9,8 +9,19 @@ for channels in channel_swizzles:
     cmd = [
         "python3",
         f"{script_dir}/wirconv.py",
-        f"--outdir=out/out-{channels}",
+        f"--outdir=out/quad/out-{channels}",
         "--indir=.",
+        f"--true-stereo-channel-swizzle={channels}",
+    ]
+    subprocess.run(cmd)
+
+for channels in channel_swizzles:
+    cmd = [
+        "python3",
+        f"{script_dir}/wirconv.py",
+        f"--outdir=out/split/out-{channels}",
+        "--indir=.",
+        "--split-true-stereo",
         f"--true-stereo-channel-swizzle={channels}",
     ]
     subprocess.run(cmd)
